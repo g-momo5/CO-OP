bb77e98f25547fac663e412b8778b6fe5f3384b4
Mohamed Ghaly <mohamed@MacBook-Air-di-Mohamed.local>
2025-11-03 19:17:28 +0100

    Update Supabase connection string to new database

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function createWindow() {
 async function initializeDatabase() {
   // PostgreSQL/Supabase configuration
   const { Client } = require('pg');
-  const connectionString = 'postgresql://postgres.lryhwpadqrlnwjinxuqi:Ghaly1997.@aws-1-eu-central-1.pooler.supabase.com:6543/postgres';
+  const connectionString = 'postgresql://postgres:Ghaly1997.@db.ihajlcodsypvjwfnkcjc.supabase.co:5432/postgres';
 
   db = new Client({
     connectionString: connectionString,
